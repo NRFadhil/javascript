e2162c2ea26bc90e01a1da404aab44bfa6728546
Nalan <nalanfadhilah1@gmail.com>
2021-09-04 20:27:58 +0700

    tugas 7 js

---
+++ tugas7.js
@@ -0,0 +1,17 @@
+function array(){
+  var tinggi=[1,2,3,4,5,6,7,8,9,10];
+  for(var i=0;i<tinggi.length;i++){
+    console.log(tinggi[i]);
+  }
+}
+function forOf(){
+  var badan=[1,2,3,4,5,6,7,8,9,10];
+for(var x of badan){
+  console.log(x);
+}
+}
+
+
+array();
+console.log("");
+forOf();
