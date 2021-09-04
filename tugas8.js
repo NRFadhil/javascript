91a00f52d4f259ebee6a57b38c77c9163a0e9cee
Nalan <nalanfadhilah1@gmail.com>
2021-09-04 20:48:57 +0700

    tugas 8 js

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function object(){
+  var laptop = {
+    type : 'asus ROG',
+    harga : 15000000,
+    warna : 'hitam',
+  }
+  console.log(laptop);
+  laptop.type='Acer';
+  console.log(laptop);
+}
+
+object();
