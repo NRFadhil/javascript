218f598ec2bbfb021e73daaf0bdf416a8e65acf9
Nalan <nalanfadhilah1@gmail.com>
2021-09-07 20:42:50 +0700

    tugas 12 js

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+function buah(){
+  var buah = ['pisang', 'Jeruk', 'Apel', 'Mangga']
+  console.log(buah);
+  buah.pop();
+  console.log(buah);
+  buah.shift();
+
+  return buah
+}
+console.log(buah());
